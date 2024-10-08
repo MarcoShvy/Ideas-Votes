ce7fb1c8ba749a9343e2e5d11a1c58647a956fe9
MarcoShvy <mass7813@gmail.com>
2024-10-08 09:05:03 -0300

    create

---
+++ src/main/java/com/example/Brainstoming/Ideas/BrainstomingIdeasApplication.java
@@ -0,0 +1,13 @@
+package com.example.Brainstoming.Ideas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainstomingIdeasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainstomingIdeasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Brainstoming/Ideas/BrainstomingIdeasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Brainstoming.Ideas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainstomingIdeasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
